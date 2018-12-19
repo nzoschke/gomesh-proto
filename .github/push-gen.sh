b81a9592eea6c70e1706b3d15d5342f2547c40ea
Noah Zoschke <noah@segment.com>
2018-12-18 16:48:40 -0800

    github action

---
+++ .github/push-gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git config user.email "gen@example.com"
+git config user.name  "gen"
+git remote -v
+
+git rm -rf .github/
+git add gen/
+git commit -m "gen ${GITHUB_SHA:0:7}"
+git push -f origin ${GITHUB_REF}:${GITHUB_REF}-gen
