fc288dd9712f06e495949dfebbde049c494b4a0f
Noah Zoschke <noah@segment.com>
2019-01-02 11:58:40 -0800

    force add and push to same branch

--- .github/push-gen.sh
+++ .github/push-gen.sh
@@ -5,7 +5,7 @@ git config user.name  "gen"
 git remote -v
 
 git rm -rf .github/
-git add gen/
+git add -f gen/
 git commit -m "gen ${GITHUB_SHA:0:7}"
 
-git push -f https://nzoschke:${PUSH_TOKEN}@github.com/nzoschke/gomesh-interface.git ${GITHUB_REF}:${GITHUB_REF}-gen
+git push -f https://nzoschke:${PUSH_TOKEN}@github.com/nzoschke/gomesh-interface.git ${GITHUB_REF}
