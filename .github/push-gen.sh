9dc832488b0bab4b2cdee17a1a27ea592ee9bc73
Noah Zoschke <noah@segment.com>
2019-01-02 10:46:47 -0800

    push script improvements

--- .github/push-gen.sh
+++ .github/push-gen.sh
@@ -2,10 +2,10 @@
 
 git config user.email "gen@example.com"
 git config user.name  "gen"
+git remote -v
 
 git rm -rf .github/
-git rm -rf gen/
 git add gen/
 git commit -m "gen ${GITHUB_SHA:0:7}"
 
-git push https://github.com/nzoschke/gomesh-interface.git ${GITHUB_REF}:${GITHUB_REF}-gen
+git push -f https://github.com/nzoschke/gomesh-interface.git ${GITHUB_REF}:${GITHUB_REF}-gen
